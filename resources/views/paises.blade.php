440ed38c2e8022e04133aad13746287b8c9ead19
jose <jose@example.com>
2022-05-03 10:34:45 -0500

    evidencia 1

--- resources/views/paises.blade.php
+++ resources/views/paises.blade.php
@@ -5,17 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Paises</title>
 </head>
 
 <body>
+    <style>
+        .pais {
+            color: red;
+        }
+
+        #capitales {
+            color: blue;
+        }
+
+        .ciudades {
+            color: gray;
+        }
+
+        .poblaciones {
+            color: lightyellow;
+        }
+
+        .cid {
+            background-color: lightgreen;
+        }
+    </style>
     <h1 align="center">Paises de la sur america</h1>
     <table class="table table-striped table-dark">
         <thead>
             <tr>
-                <th>Pais</th>
+                <th >Pais</th>
                 <th>Capital</th>
                 <th>Moneda</th>
                 <th>Poblacion</th>
@@ -25,20 +46,20 @@
         <tbody>
             @foreach($paises as $pais => $infopais)
             <tr>
-                <td rowspan="{{ count($infopais['ciudades'])}}">
+                <td class="pais" rowspan="{{ count($infopais['ciudades'])}}">
                     {{ $pais }}
                 </td>
-                <td rowspan="{{ count($infopais['ciudades'])}}">
+                <td id="capitales" rowspan="{{ count($infopais['ciudades'])}}">
                     {{ $infopais["capital"] }}
                 </td>
-                <td rowspan="{{ count($infopais['ciudades'])}}">
+                <td class="ciudades" rowspan="{{ count($infopais['ciudades'])}}">
                     {{ $infopais["moneda"] }}
                 </td>
-                <td rowspan="{{ count($infopais['ciudades'])}}">
+                <td class="poblaciones" rowspan="{{ count($infopais['ciudades'])}}">
                     {{ $infopais["poblacion"] }}
                 </td>
                 @foreach($infopais["ciudades"] as $ciudad )
-                <td>
+                <td class="cid">
                     {{ $ciudad }}
                 </td>
             </tr>
